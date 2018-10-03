dbbd8ca5d2da256c2f651c756773f2cf239188b7
Andres <andres.ac13@gmail.com>
2018-10-03 17:40:21 -0300

    Cambio listBox por DGV

--- LigaFutbolRegional/JugadoresXCampeonato.Designer.vb
+++ LigaFutbolRegional/JugadoresXCampeonato.Designer.vb
@@ -22,75 +22,214 @@ Partial Class JugadoresXCampeonato
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lista_elementos = New System.Windows.Forms.ListBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.btn_agregar = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dni = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.dni2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nombre2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.JugadorBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DataSet1 = New LigaFutbolRegional.DataSet1()
+        Me.JugadorTableAdapter = New LigaFutbolRegional.DataSet1TableAdapters.JugadorTableAdapter()
+        Me.contador = New System.Windows.Forms.Label()
+        Me.lbl_count = New System.Windows.Forms.Label()
+        Me.ComboBox_011 = New LigaFutbolRegional.ComboBox_01()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txt_nuevo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.JugadorBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'lista_elementos
+        'DataGridView1
         '
-        Me.lista_elementos.FormattingEnabled = True
-        Me.lista_elementos.Location = New System.Drawing.Point(109, 114)
-        Me.lista_elementos.Name = "lista_elementos"
-        Me.lista_elementos.Size = New System.Drawing.Size(120, 95)
-        Me.lista_elementos.TabIndex = 0
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.dni, Me.nombre})
+        Me.DataGridView1.Location = New System.Drawing.Point(65, 107)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(243, 235)
+        Me.DataGridView1.TabIndex = 5
         '
-        'ListBox2
+        'dni
         '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(320, 114)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(120, 95)
-        Me.ListBox2.TabIndex = 1
+        Me.dni.HeaderText = "DNI"
+        Me.dni.Name = "dni"
         '
-        'btn_agregar
+        'nombre
         '
-        Me.btn_agregar.Location = New System.Drawing.Point(274, 273)
-        Me.btn_agregar.Name = "btn_agregar"
-        Me.btn_agregar.Size = New System.Drawing.Size(75, 23)
-        Me.btn_agregar.TabIndex = 2
-        Me.btn_agregar.Text = "Button1"
-        Me.btn_agregar.UseVisualStyleBackColor = True
+        Me.nombre.HeaderText = "Nombre"
+        Me.nombre.Name = "nombre"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AllowUserToDeleteRows = False
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.dni2, Me.nombre2})
+        Me.DataGridView2.Location = New System.Drawing.Point(383, 107)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.ReadOnly = True
+        Me.DataGridView2.Size = New System.Drawing.Size(243, 235)
+        Me.DataGridView2.TabIndex = 6
+        '
+        'dni2
+        '
+        Me.dni2.HeaderText = "DNI"
+        Me.dni2.Name = "dni2"
+        Me.dni2.ReadOnly = True
+        '
+        'nombre2
+        '
+        Me.nombre2.HeaderText = "Nombre"
+        Me.nombre2.Name = "nombre2"
+        Me.nombre2.ReadOnly = True
+        '
+        'JugadorBindingSource
+        '
+        Me.JugadorBindingSource.DataMember = "Jugador"
+        Me.JugadorBindingSource.DataSource = Me.DataSet1
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "DataSet1"
+        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'JugadorTableAdapter
+        '
+        Me.JugadorTableAdapter.ClearBeforeFill = True
+        '
+        'contador
+        '
+        Me.contador.AutoSize = True
+        Me.contador.Location = New System.Drawing.Point(92, 360)
+        Me.contador.Name = "contador"
+        Me.contador.Size = New System.Drawing.Size(55, 13)
+        Me.contador.TabIndex = 7
+        Me.contador.Text = "Cantidad: "
+        '
+        'lbl_count
+        '
+        Me.lbl_count.AutoSize = True
+        Me.lbl_count.Location = New System.Drawing.Point(154, 360)
+        Me.lbl_count.Name = "lbl_count"
+        Me.lbl_count.Size = New System.Drawing.Size(13, 13)
+        Me.lbl_count.TabIndex = 8
+        Me.lbl_count.Text = "0"
+        '
+        'ComboBox_011
+        '
+        Me.ComboBox_011.FormattingEnabled = True
+        Me.ComboBox_011.Location = New System.Drawing.Point(119, 40)
+        Me.ComboBox_011.Name = "ComboBox_011"
+        Me.ComboBox_011.nombre_campo = Nothing
+        Me.ComboBox_011.nombre_tabla = Nothing
+        Me.ComboBox_011.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_011.TabIndex = 9
+        Me.ComboBox_011.validable = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(138, 82)
+        Me.Label1.Location = New System.Drawing.Point(64, 47)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(28, 13)
-        Me.Label1.TabIndex = 3
+        Me.Label1.TabIndex = 10
         Me.Label1.Text = "Club"
         '
-        'txt_nuevo
+        'Label2
         '
-        Me.txt_nuevo.Location = New System.Drawing.Point(129, 273)
-        Me.txt_nuevo.Name = "txt_nuevo"
-        Me.txt_nuevo.Size = New System.Drawing.Size(100, 20)
-        Me.txt_nuevo.TabIndex = 4
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(488, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Jugadores Disponibles"
+        '
+        'Label3
+        '
+        Me.Label3.AutoEllipsis = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(130, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Jugadores Agregados"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(108, 409)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(526, 409)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Cancelar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(233, 409)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Borrar"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'JugadoresXCampeonato
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightSeaGreen
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.txt_nuevo)
+        Me.ClientSize = New System.Drawing.Size(645, 462)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.btn_agregar)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.lista_elementos)
+        Me.Controls.Add(Me.ComboBox_011)
+        Me.Controls.Add(Me.lbl_count)
+        Me.Controls.Add(Me.contador)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "JugadoresXCampeonato"
         Me.Text = "JugadoresXCampeonato"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.JugadorBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents lista_elementos As ListBox
-    Friend WithEvents ListBox2 As ListBox
-    Friend WithEvents btn_agregar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents DataSet1 As DataSet1
+    Friend WithEvents JugadorBindingSource As BindingSource
+    Friend WithEvents JugadorTableAdapter As DataSet1TableAdapters.JugadorTableAdapter
+    Friend WithEvents dni As DataGridViewTextBoxColumn
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents dni2 As DataGridViewTextBoxColumn
+    Friend WithEvents nombre2 As DataGridViewTextBoxColumn
+    Friend WithEvents contador As Label
+    Friend WithEvents lbl_count As Label
+    Friend WithEvents ComboBox_011 As ComboBox_01
     Friend WithEvents Label1 As Label
-    Friend WithEvents txt_nuevo As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- LigaFutbolRegional/JugadoresXCampeonato.vb
+++ LigaFutbolRegional/JugadoresXCampeonato.vb
@@ -3,42 +3,55 @@
     Dim _BD As New CONEXION_BD
 
 
-    Private Sub btn_agregar_Click(sender As Object, e As EventArgs) Handles btn_agregar.Click
-        If Me.txt_nuevo.Text <> "" Then
-            inserta_elemento(Me.txt_nuevo.Text)
-
-
-
-            Me.txt_nuevo.Text = ""
-        End If
-    End Sub
-    Sub inserta_elemento(ByVal valor As String)
-
-        Me.lista_elementos.Items.Add(valor)
-    End Sub
-
-
-    Private Sub cargar_jugadores()
+    Private Sub cargarGrilla()
         Dim tabla As New DataTable
-        tabla = _BD.leo_tabla("SELECT nombre FROM Jugador")
+        tabla = _BD.leo_tabla("Select dni, nombre from Jugador")
+
         Dim aux As Integer = tabla.Rows.Count - 1
         Dim valor As Boolean = True
         While valor
-            ListBox2.Items.Add(tabla.Rows(aux)("nombre"))
+            Me.DataGridView2.Rows.Add(tabla.Rows(aux)("dni"), tabla.Rows(aux)("nombre"))
             aux -= 1
             If (aux = -1) Then
                 valor = False
             End If
         End While
 
-    End Sub
 
+    End Sub
     Private Sub JugadoresXCampeonato_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cargar_jugadores()
+        cargarGrilla()
+
+
     End Sub
 
-    Private Sub ListBox2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox2.MouseDoubleClick
-        lista_elementos.Items.Add(ListBox2.SelectedItem)
-        ListBox2.Items.Remove(ListBox2.SelectedItem)
+
+
+    Private Sub DataGridView2_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentDoubleClick
+        Dim dni As Integer
+        Dim nombre As String
+        Dim tabla As New DataTable
+        dni = DataGridView2(0, DataGridView2.CurrentRow.Index).Value
+        nombre = DataGridView2(1, DataGridView2.CurrentRow.Index).Value
+
+        Me.DataGridView1.Rows.Add(dni, nombre)
+
+        Me.DataGridView2.Rows.RemoveAt(DataGridView2.CurrentRow.Index)
+
+        lbl_count.Text = Me.DataGridView1.Rows.Count - 1
+    End Sub
+
+    Private Sub DataGridView1_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentDoubleClick
+        Dim dni As Integer
+        Dim nombre As String
+        Dim tabla As New DataTable
+        dni = DataGridView1(0, DataGridView1.CurrentRow.Index).Value
+        nombre = DataGridView1(1, DataGridView1.CurrentRow.Index).Value
+
+        Me.DataGridView2.Rows.Add(dni, nombre)
+
+        Me.DataGridView1.Rows.RemoveAt(DataGridView1.CurrentRow.Index)
+
+        lbl_count.Text = Me.DataGridView1.Rows.Count - 1
     End Sub
 End Class
